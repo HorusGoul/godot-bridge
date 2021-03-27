860e482edc398985ada2c47b2054743afd9910f9
Horus Lugo <horusgoul@gmail.com>
2021-03-27 18:21:06 +0100

    feat: move vite-template to another repo

--- packages/vite-template/gui/src/BridgeContext.tsx
+++
@@ -1,8 +0,0 @@
-import { GodotBridge } from "godot-bridge";
-import * as React from "react";
-
-export const BridgeContext = React.createContext<GodotBridge>(null!);
-
-export function useBridge() {
-  return React.useContext(BridgeContext);
-}
