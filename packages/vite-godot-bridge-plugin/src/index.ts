a60721656473fcc13d3c0a1892dc441ee9730a78
Horus Lugo <horusgoul@gmail.com>
2021-03-27 12:45:56 +0100

    feat: add vite-godot-bridge-plugin

---
+++ packages/vite-godot-bridge-plugin/src/index.ts
@@ -0,0 +1,48 @@
+import type { Plugin, ViteDevServer } from 'vite';
+import type { GodotBridge } from 'godot-bridge';
+
+export default function godotBridgePlugin(): Plugin {
+  let server: ViteDevServer;
+
+  return {
+    name: 'godot-bridge',
+
+    configureServer(_server) {
+      server = _server;
+
+      server.middlewares.use((req, res, next) => {
+        if (req.method === 'POST' && req.originalUrl === '/__godot_refresh') {
+          server.ws.send({
+            type: 'custom',
+            event: 'godot-update',
+          });
+
+          res.statusCode = 200;
+          res.end();
+          return;
+        }
+
+        next();
+      });
+    },
+  };
+}
+
+export function configureGodotBridgeViteClient({
+  bridge,
+  engine,
+}: {
+  bridge: GodotBridge;
+  engine: Engine;
+}) {
+  if (import.meta.hot) {
+    import.meta.hot?.on('godot-update', async () => {
+      engine.requestQuit();
+
+      bridge.teardown();
+      await bridge.setup();
+
+      await engine.startGame();
+    });
+  }
+}
